7b2fdd29cb6985cba87020e03d9aa6515e0f42cb
feliks <547276540@qq.com>
2017-03-17 10:19:19 +0800

    Add YunTongXun

--- src/Gateways/YunTongXunGateway.php
+++ src/Gateways/YunTongXunGateway.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of the overtrue/easy-sms.
- * (c) wwp66650 <wwp66650@gmail.com>
+ * (c) overtrue <i@overtrue.me>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
@@ -17,23 +17,6 @@ class YunTongXunGateway extends Gateway
 
     const ENDPOINT_TEMPLATE = 'https://%s:%s/%s/%s/%s/%s/%s?sig=%s';
     const SDK_VERSION = '2013-12-26';
-
-    protected $server_ip;
-    protected $server_port;
-    protected $account_sid;
-
-    /**
-     * YunTongXunGateway constructor.
-     *
-     * @param array $config
-     */
-    public function __construct(array $config)
-    {
-        $this->server_ip = $config['server_ip'];
-        $this->server_port = $config['server_port'];
-        $this->account_sid = $config['account_sid'];
-        parent::__construct($config);
-    }
 
     /**
      * Send a short message.
@@ -60,7 +43,7 @@ class YunTongXunGateway extends Gateway
             'headers' => [
                 "Accept" => 'application/json',
                 "Content-Type" => 'application/json;charset=utf-8',
-                "Authorization" => base64_encode($this->account_sid . ":" . $datetime),
+                "Authorization" => base64_encode($this->config->get('account_sid') . ":" . $datetime),
             ],
         ]);
     }
@@ -77,9 +60,9 @@ class YunTongXunGateway extends Gateway
     protected function buildEndpoint($type, $resource, $datetime)
     {
         $account_type = $this->config->get('is_sub_account') ? 'SubAccounts' : 'Accounts';
-        // 大写的sig参数
-        $sig = strtoupper(md5($this->account_sid . $this->config->get('account_token') . $datetime));
 
-        return sprintf(self::ENDPOINT_TEMPLATE, $this->server_ip, $this->server_port, self::SDK_VERSION, $account_type, $this->account_sid, $type, $resource, $sig);
+        $sig = strtoupper(md5($this->config->get('account_sid') . $this->config->get('account_token') . $datetime));
+
+        return sprintf(self::ENDPOINT_TEMPLATE, $this->config->get('server_ip'), $this->config->get('server_port'), self::SDK_VERSION, $account_type, $this->config->get('account_sid'), $type, $resource, $sig);
     }
 }
--- tests/Gateways/YunTongXunGatewayTest.php
+++ tests/Gateways/YunTongXunGatewayTest.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of the overtrue/easy-sms.
- * (c) wwp66650 <wwp66650@gmail.com>
+ * (c) overtrue <i@overtrue.me>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
