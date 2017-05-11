bb805e105a41f32d3a9d8447d59b0d60e24d736d
overtrue <anzhengchao@gmail.com>
2017-05-11 20:43:18 +0800

    :sparkles: Add SendCloud.

---
+++ src/Gateways/SendcloudGateway.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the overtrue/easy-sms.
+ * (c) overtrue <i@overtrue.me>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\EasySms\Gateways;
+
+use Overtrue\EasySms\Contracts\MessageInterface;
+use Overtrue\EasySms\Exceptions\GatewayErrorException;
+use Overtrue\EasySms\Support\Config;
+use Overtrue\EasySms\Traits\HasHttpRequest;
+
+/**
+ * Class SendcloudGateway.
+ *
+ * @see http://sendcloud.sohu.com/doc/sms/
+ */
+class SendcloudGateway extends Gateway
+{
+    use HasHttpRequest;
+
+    const ENDPOINT_TEMPLATE = 'http://www.sendcloud.net/smsapi/%s';
+
+    /**
+     * Send a short message.
+     *
+     * @param int|string|array                             $to
+     * @param \Overtrue\EasySms\Contracts\MessageInterface $message
+     * @param \Overtrue\EasySms\Support\Config             $config
+     *
+     * @return array
+     */
+    public function send($to, MessageInterface $message, Config $config)
+    {
+        $params = [
+            'smsUser' => $config->get('sms_user'),
+            'templateId' => $message->getTemplate(),
+            'phone' => is_array($to) ? join(',', $to) : $to,
+            'vars' => $this->formatTemplateVars($message->getData()),
+            'timestamp' => time(),
+        ];
+
+        $params['signature'] = $this->sign($params, $config->get('sms_key'));
+
+        $result = $this->post(sprintf(self::ENDPOINT_TEMPLATE, 'send'), $params);
+
+        if (!$result['result']) {
+            throw new GatewayErrorException($result['message'], $result['statusCode'], $result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $vars
+     *
+     * @return string
+     */
+    protected function formatTemplateVars(array $vars)
+    {
+        $formatted = [];
+
+        foreach ($vars as $key => $value) {
+            $formatted[sprintf('%%%s%%', trim($key, '%'))] = $value;
+        }
+
+        return json_encode($formatted);
+    }
+
+    /**
+     * @param string $params
+     * @param string $key
+     *
+     * @return string
+     */
+    protected function sign($params, $key)
+    {
+        ksort($params);
+
+        return md5(sprintf('%s&%s&%s', $key, urldecode(http_build_query($params)), $key));
+    }
+}
