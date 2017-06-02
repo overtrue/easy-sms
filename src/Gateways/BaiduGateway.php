4e3c5295a032c8839944c6ccfc84a239135483cc
iwzh <wzhec@foxmail.com>
2017-06-02 18:04:53 +0800

    精简过滤签名getHeadersToSign的方法，headers确定，指定的keys确定，兼容写法可以全部删除。 精简getCanonicalHeaders方法，无空值，兼容代码删除
    Signed-off-by: iwzh <wzhec@foxmail.com>

--- src/Gateways/BaiduGateway.php
+++ src/Gateways/BaiduGateway.php
@@ -15,7 +15,7 @@ use Overtrue\EasySms\Support\Config;
 use Overtrue\EasySms\Traits\HasHttpRequest;
 
 /**
- * Class BaiduGateway
+ * Class BaiduGateway.
  *
  * @see https://cloud.baidu.com/doc/SMS/API.html
  */
@@ -27,10 +27,10 @@ class BaiduGateway extends Gateway
     const ENDPOINT_URI = '/bce/v2/message';
     const BCE_AUTH_VERSION = 'bce-auth-v1';
     const DEFAULT_EXPIRATION_IN_SECONDS = 1800; //签名有效期默认1800秒
-    const SUCCESS_CODE=1000;
-    
+    const SUCCESS_CODE = 1000;
+
     /**
-     * Send  message.
+     * Send message.
      *
      * @param array|int|string                             $to
      * @param \Overtrue\EasySms\Contracts\MessageInterface $message
@@ -43,145 +43,117 @@ class BaiduGateway extends Gateway
     public function send($to, MessageInterface $message, Config $config)
     {
         $params = [
-            'invoke_id' => $config->get('invoke_id'),
+            'invokeId' => $config->get('invoke_id'),
             'phoneNumber' => $to,
             'templateCode' => $message->getTemplate(),
-            'contentVar' => $message->getData()
+            'contentVar' => $message->getData(),
         ];
-        //默认值当前时间
-        date_default_timezone_set("PRC");
-        $timestamp = new \DateTime();
-        $timestamp->setTimezone(new \DateTimeZone("UTC"));
-        $datetime = $timestamp->format("Y-m-d\TH:i:s\Z");
-        //生成json格式
-        $headers = ['Host'=>self::ENDPOINT_HOST,"Content-Type" => 'application/json',  'x-bce-date' => $datetime, 'x-bce-content-sha256' => hash('sha256', json_encode($params))];
-        $headersToSign =array('host', 'x-bce-content-sha256',);//需要签名的header头
-        $headers['Authorization']=$this->generateSign($headersToSign, $datetime,$headers,$config);
-                
-        $result=$this->request('post',self::buildEndpoint(), ['headers' => $headers, 'json' => $params]);
-        
-        if($result['code']!=self::SUCCESS_CODE){
-            throw new GatewayErrorException($result['message'],$result['code'],$result);
+
+        $datetime = date('Y-m-d\TH:i:s\Z');
+
+        $headers = [
+            'host' => self::ENDPOINT_HOST,
+            'content-type' => 'application/json',
+            'x-bce-date' => $datetime,
+            'x-bce-content-sha256' => hash('sha256', json_encode($params)),
+        ];
+        //获得需要签名的数据
+        $signHeaders = $this->getHeadersToSign($headers, ['host', 'x-bce-content-sha256']);
+
+        $headers['Authorization'] = $this->generateSign($signHeaders, $datetime, $config);
+
+        $result = $this->request('post', self::buildEndpoint($config), ['headers' => $headers, 'json' => $params]);
+
+        if ($result['code'] != self::SUCCESS_CODE) {
+            throw new GatewayErrorException($result['message'], $result['code'], $result);
         }
+
         return $result;
     }
 
     /**
      * Build endpoint url.
      *
+     * @param \Overtrue\EasySms\Support\Config $config
+     *
      * @return string
      */
-    public static function buildEndpoint()
+    protected function buildEndpoint(Config $config)
     {
-        return 'http://'.self::ENDPOINT_HOST.self::ENDPOINT_URI;
-    }
-    
-    /**
-     * generate Authorization.
-     *
-     * @param $headersToSign
-     * @param $datetime
-     * @param $headers
-     * @param Config $config
-     * @return string
-     */
-    public function generateSign($headersToSign, $datetime, $headers, Config $config)
-    {
-        //生成authString
-        $authString = self::BCE_AUTH_VERSION . '/' . $config->get('ak') . '/'
-            . $datetime . '/' . self::DEFAULT_EXPIRATION_IN_SECONDS;
-        //使用sk和authString生成signKey
-        $signingKey = hash_hmac('sha256', $authString, $config->get('sk'));
-        //生成标准化URI
-        $canonicalURI = str_replace('%2F', '/', rawurlencode(self::ENDPOINT_URI));// 根据RFC 3986，除了：1.大小写英文字符2.阿拉伯数字3.点'.'、波浪线'~'、减号'-'以及下划线'_' 以外都要编码
-        
-        //生成标准化QueryString
-        $canonicalQueryString = '';//此api不需要此项。返回空字符串
-        
-        $signHeaders = self::getHeadersToSign($headers, $headersToSign);//获得需要签名的数据
-        //整理headersToSign，以';'号连接
-        $signedHeaders = empty($signHeaders) ? '' : strtolower(trim(implode(";", array_keys($signHeaders))));
-        //生成标准化header
-        $canonicalHeader = self::getCanonicalHeaders($signHeaders);
-        //组成标准请求串
-        $canonicalRequest = "POST\n$canonicalURI\n" . "$canonicalQueryString\n$canonicalHeader";
-        //使用signKey和标准请求串完成签名
-        $signature = hash_hmac('sha256', $canonicalRequest, $signingKey);
-        
-        //组成最终签名串
-        $authorizationHeader = "$authString/$signedHeaders/$signature";
-        return $authorizationHeader;
-    }
-    
-    /**
-     * 生成标准化http请求头串
-     *
-     * @param $headers
-     * @return string
-     */
-    public static function getCanonicalHeaders($headers)
-    {
-        //如果没有headers，则返回空串
-        if (count($headers) == 0) {
-            return '';
-        }
-        
-        $headerStrings = array();
-        foreach ($headers as $k => $v) {
-            //跳过key为null的
-            if ($k === null) {
-                continue;
-            }
-            //如果value为null，则赋值为空串
-            if ($v === null) {
-                $v = '';
-            }
-            //trim后再encode，之后使用':'号连接起来
-            $headerStrings[] = rawurlencode((strtolower(trim($k)))) . ':' . rawurlencode((trim($v)));
-        }
-        //字典序排序
-        sort($headerStrings);
-        
-        //用'\n'把它们连接起来
-        return implode("\n", $headerStrings);
-    }
-    
-    /**
-     * 根据headsToSign过滤应该参与签名的header
-     *
-     * @param $headers
-     * @param $headersToSign
-     * @return array
-     */
-    public static function getHeadersToSign($headers, $headersToSign)
-    {
-        //value被trim后为空串的header不参与签名
-        $filter_empty = function ($v) {
-            return trim((string)$v) !== '';
-        };
-        $headers = array_filter($headers, $filter_empty);
-        
-        //处理headers的key：去掉前后的空白并转化成小写
-        $trim_and_lower = function ($str) {
-            return strtolower(trim($str));
-        };
-        $temp = array();
-        $process_keys = function ($k, $v) use (&$temp, $trim_and_lower) {
-            $temp[$trim_and_lower($k)] = $v;
-        };
-        array_map($process_keys, array_keys($headers), $headers);
-        $headers = $temp;
-        
-        //取出headers的key以备用
-        $header_keys = array_keys($headers);
-        //预处理headersToSign：去掉前后的空白并转化成小写
-        $headersToSign = array_map($trim_and_lower, $headersToSign);
-        
-        //只选取在headersToSign里面的header
-        $filtered_keys = array_intersect($header_keys, $headersToSign);
-        
-        //返回需要参与签名的header
-        return array_intersect_key($headers, array_flip($filtered_keys));
+        return 'http://'.$config->get('domain', self::ENDPOINT_HOST).self::ENDPOINT_URI;
     }
 
+    /**
+     * Generate Authorization header.
+     *
+     * @param array                            $signHeaders
+     * @param int                              $datetime
+     * @param \Overtrue\EasySms\Support\Config $config
+     *
+     * @return string
+     */
+    protected function generateSign(array $signHeaders, $datetime, Config $config)
+    {
+        // 生成 authString
+        $authString = self::BCE_AUTH_VERSION.'/'.$config->get('ak').'/'
+            .$datetime.'/'.self::DEFAULT_EXPIRATION_IN_SECONDS;
+
+        // 使用 sk 和 authString 生成 signKey
+        $signingKey = hash_hmac('sha256', $authString, $config->get('sk'));
+        // 生成标准化 URI
+        // 根据 RFC 3986，除了：1.大小写英文字符 2.阿拉伯数字 3.点'.'、波浪线'~'、减号'-'以及下划线'_' 以外都要编码
+        $canonicalURI = str_replace('%2F', '/', rawurlencode(self::ENDPOINT_URI));
+
+        // 生成标准化 QueryString
+        $canonicalQueryString = ''; // 此 api 不需要此项。返回空字符串
+
+        // 整理 headersToSign，以 ';' 号连接
+        $signedHeaders = empty($signHeaders) ? '' : strtolower(trim(implode(';', array_keys($signHeaders))));
+
+        // 生成标准化 header
+        $canonicalHeader = $this->getCanonicalHeaders($signHeaders);
+
+        // 组成标准请求串
+        $canonicalRequest = "POST\n{$canonicalURI}\n{$canonicalQueryString}\n{$canonicalHeader}";
+
+        // 使用 signKey 和标准请求串完成签名
+        $signature = hash_hmac('sha256', $canonicalRequest, $signingKey);
+
+        // 组成最终签名串
+        return "{$authString}/{$signedHeaders}/{$signature}";
+    }
+
+    /**
+     * 生成标准化 http 请求头串.
+     *
+     * @param array $headers
+     *
+     * @return string
+     */
+    protected function getCanonicalHeaders(array $headers)
+    {
+        $headerStrings = [];
+        foreach ($headers as $name => $value) {
+            //trim后再encode，之后使用':'号连接起来
+            $headerStrings[] = rawurlencode(strtolower(trim($name))).':'.rawurlencode(trim($value));
+        }
+
+        sort($headerStrings);
+
+        return implode("\n", $headerStrings);
+    }
+
+    /**
+     * 根据 指定的 keys 过滤应该参与签名的 header.
+     *
+     * @param array $headers
+     * @param array $keys
+     *
+     * @return array
+     */
+    protected function getHeadersToSign(array $headers, array $keys)
+    {
+        //keys确定，headers确定，兼容写法删除
+        return array_intersect_key($headers, array_flip($keys));
+    }
 }
