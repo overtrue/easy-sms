5d5c22717f49f6b2dbf25daa0021b8d4a8a9f8d0
carson <wanganlin@ecmoban.com>
2017-07-04 17:40:22 +0800

    增加 AliyunGateway

---
+++ src/Gateways/AliyunGateway.php
@@ -0,0 +1,140 @@
+<?php
+
+/*
+ * This file is part of the overtrue/easy-sms.
+ * (c) carson <docxcn@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\EasySms\Gateways;
+
+use Overtrue\EasySms\Contracts\MessageInterface;
+use Overtrue\EasySms\Exceptions\GatewayErrorException;
+use Overtrue\EasySms\Support\Config;
+use Overtrue\EasySms\Traits\HasHttpRequest;
+
+/**
+ * Class AliyunGateway.
+ *
+ * @see https://help.aliyun.com/document_detail/55451.html
+ */
+class AliyunGateway extends Gateway
+{
+    use HasHttpRequest;
+
+    const ENDPOINT_URL = 'http://dysmsapi.aliyuncs.com';
+    const ENDPOINT_METHOD = 'SendSms';
+    const ENDPOINT_VERSION = '2017-05-25';
+    const ENDPOINT_FORMAT = 'JSON';
+    const ENDPOINT_REGION_ID = 'cn-hangzhou';
+    const ENDPOINT_SIGNATURE_METHOD = 'HMAC-SHA1';
+    const ENDPOINT_SIGNATURE_VERSION = '1.0';
+
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+        date_default_timezone_set('GMT');
+    }
+
+    /**
+     * @param array|int|string $to
+     * @param \Overtrue\EasySms\Contracts\MessageInterface $message
+     * @param \Overtrue\EasySms\Support\Config $config
+     *
+     * @return array
+     *
+     * @throws \Overtrue\EasySms\Exceptions\GatewayErrorException;
+     */
+    public function send($to, MessageInterface $message, Config $config)
+    {
+        $params = [
+            'RegionId' => self::ENDPOINT_REGION_ID,
+            'AccessKeyId' => $config->get('access_key_id'),
+            'Format' => self::ENDPOINT_FORMAT,
+            'SignatureMethod' => self::ENDPOINT_SIGNATURE_METHOD,
+            'SignatureVersion' => self::ENDPOINT_SIGNATURE_VERSION,
+            'SignatureNonce' => uniqid(),
+            'Timestamp' => date('Y-m-d\TH:i:s\Z'),
+            'Action' => self::ENDPOINT_METHOD,
+            'Version' => self::ENDPOINT_VERSION,
+            'PhoneNumbers' => strval($to),
+            'SignName' => $config->get('sign_name'),
+            'TemplateCode' => $message->getTemplate(),
+            'TemplateParam' => json_encode($message->getData()),
+        ];
+
+        $params['Signature'] = $this->generateSign($params);
+
+        $result = $this->get(self::ENDPOINT_URL, $params);
+
+        if ($result['Code'] != 'OK') {
+            throw new GatewayErrorException($result['Message'], $result['Code'], $result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generate Sign.
+     *
+     * @param array $params
+     *
+     * @return string
+     */
+    protected function generateSign($params)
+    {
+        ksort($params);
+        $accessKeySecret = $this->config->get('access_key_secret');
+        $canonicalizedQueryString = '';
+        foreach ($params as $key => $value) {
+            $canonicalizedQueryString .= '&' . $this->percentEncode($key) . '=' . $this->percentEncode($value);
+        }
+        $stringToSign = 'GET' . '&%2F&' . $this->percentencode(substr($canonicalizedQueryString, 1));
+
+        return $this->signString($stringToSign, $accessKeySecret . "&");
+    }
+
+    /**
+     * @param $source
+     * @param $accessSecret
+     *
+     * @return string
+     */
+    protected function signString($source, $accessSecret)
+    {
+        return base64_encode(hash_hmac('sha1', $source, $accessSecret, true));
+    }
+
+    /**
+     * @param $value
+     *
+     * @return string
+     */
+    protected function prepareValue($value)
+    {
+        if (is_bool($value)) {
+            if ($value) {
+                return "true";
+            } else {
+                return "false";
+            }
+        } else {
+            return $value;
+        }
+    }
+
+    /**
+     * @param $str
+     *
+     * @return mixed|string
+     */
+    protected function percentEncode($str)
+    {
+        $res = urlencode($str);
+        $res = preg_replace('/\+/', '%20', $res);
+        $res = preg_replace('/\*/', '%2A', $res);
+        $res = preg_replace('/%7E/', '~', $res);
+        return $res;
+    }
+}
