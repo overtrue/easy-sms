8190d0b0c0b198ddeca0be6127d319a6dfd97b7c
Darren Gao <realgaodacheng@gmail.com>
2017-12-22 14:28:39 +0800

    add comment.

--- src/Gateways/RongcloudGateway.php
+++ src/Gateways/RongcloudGateway.php
@@ -107,6 +107,7 @@ class RongcloudGateway extends Gateway
      * Generate Sign.
      *
      * @param array $params
+     * @param \Overtrue\EasySms\Support\Config $config
      *
      * @return string
      */
