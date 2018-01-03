f7b782bf1263591e772d82ce1336c202b4b3df8a
tradzero <admin@drakframe.com>
2018-01-03 15:07:20 +0800

    测试

---
+++ tests/Gateways/TwilioGatewayTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the overtrue/easy-sms.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\EasySms\Tests\Gateways;
+
+use Overtrue\EasySms\Exceptions\GatewayErrorException;
+use Overtrue\EasySms\Gateways\TwilioGateway;
+use Overtrue\EasySms\Message;
+use Overtrue\EasySms\Support\Config;
+use Overtrue\EasySms\Tests\TestCase;
+
+class TwilioGatewayTest extends TestCase
+{
+    public function testGetName()
+    {
+        $this->assertSame('twilio', (new TwilioGateway([]))->getName());
+    }
+
+    public function testSend()
+    {
+        $config = [
+            'account_sid' => 'mock-api-account-sid',
+            'from' => 'mock-from',
+            'token' => 'mock-token',
+        ];
+        $gateway = \Mockery::mock(TwilioGateway::class.'[request]', [$config])->shouldAllowMockingProtectedMethods();
+
+        $gateway->shouldReceive('request')->andReturn([
+            'status' => 'queued',
+            'from' => 'mock-from',
+            'to' => '+8618888888888',
+            'body' => '【twilio】This is a test message.',
+            'sid' => 'mock-api-account-sid',
+            'error_code' => null,
+        ]);
+
+        $message = new Message(['content' => '【twilio】This is a test message.']);
+        $config = new Config($config);
+
+        $this->assertSame([
+            'status' => 'queued',
+            'from' => 'mock-from',
+            'to' => '+8618888888888',
+            'body' => '【twilio】This is a test message.',
+            'sid' => 'mock-api-account-sid',
+            'error_code' => null,
+        ], $gateway->send('+8618888888888', $message, $config));
+    }
+}
