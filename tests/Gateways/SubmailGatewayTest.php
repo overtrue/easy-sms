febe6ef47f68fc70dd87c1462b6c64ba8fa701d3
安正超 <anzhengchao@gmail.com>
2018-08-10 15:18:55 +0800

    Update SubmailGatewayTest.php

--- tests/Gateways/SubmailGatewayTest.php
+++ tests/Gateways/SubmailGatewayTest.php
@@ -62,4 +62,46 @@ class SubmailGatewayTest extends TestCase
 
         $gateway->send(new PhoneNumber(18188888888), $message, $config);
     }
+    
+    public function testProject() {
+        $config = [
+            'app_id' => 'mock-app-id',
+            'app_key' => 'mock-app-key',
+            // no project id
+        ];
+        $gateway = \Mockery::mock(SubmailGateway::class.'[post]', [$config])->shouldAllowMockingProtectedMethods();
+
+        $gateway->shouldReceive('post')->with('https://api.mysubmail.com/message/xsend.json', [
+            'appid' => 'mock-app-id',
+            'signature' => 'mock-app-key',
+            'project' => 'mock-project',
+            'to' => new PhoneNumber(18188888888),
+            'vars' => json_encode(['code' => '123456', 'time' => '15']),
+        ])->andReturn([
+            'status' => 'success',
+            'send_id' => '093c0a7df143c087d6cba9cdf0cf3738',
+            'fee' => 1,
+            'sms_credits' => 14197,
+        ], [
+            'status' => 'error',
+            'code' => 100,
+            'msg' => 'mock-err-msg',
+        ])->times(2);
+
+        $message = new Message(['data' => ['code' => '123456', 'time' => '15', 'project' => 'mock-project']]);
+        $config = new Config($config);
+
+        $this->assertSame([
+            'status' => 'success',
+            'send_id' => '093c0a7df143c087d6cba9cdf0cf3738',
+            'fee' => 1,
+            'sms_credits' => 14197,
+        ], $gateway->send(new PhoneNumber(18188888888), $message, $config));
+
+        $this->expectException(GatewayErrorException::class);
+        $this->expectExceptionCode(100);
+        $this->expectExceptionMessage('mock-err-msg');
+
+        $gateway->send(new PhoneNumber(18188888888), $message, $config);
+    }
 }
