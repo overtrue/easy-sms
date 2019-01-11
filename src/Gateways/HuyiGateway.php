b329e37ca738092e3fb69dffe37f0cc0e95d8567
srt <sgssxf@outlook.com>
2019-01-11 09:51:15 +0800

    Update HuyiGateway.php

--- src/Gateways/HuyiGateway.php
+++ src/Gateways/HuyiGateway.php
@@ -49,7 +49,7 @@ class HuyiGateway extends Gateway
             'content' => $message->getContent($this),
             'time' => time(),
             'format' => self::ENDPOINT_FORMAT,
-            'sign' => $config->get('sign_name'),
+            'sign' => $config->get('signature'),
         ];
 
         $params['password'] = $this->generateSign($params);
