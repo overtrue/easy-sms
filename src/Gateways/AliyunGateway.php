09598d18049ae3e8d43894a2d676ff67d2583470
安正超 <anzhengchao@gmail.com>
2019-03-22 11:26:10 +0800

    Update AliyunGateway.php

--- src/Gateways/AliyunGateway.php
+++ src/Gateways/AliyunGateway.php
@@ -55,7 +55,7 @@ class AliyunGateway extends Gateway
     {
         $data = $message->getData($this);
         
-        $signName = $data['sign_name'] ?? $config->get('sign_name');
+        $signName = !empty($data['sign_name']) ? $data['sign_name'] : $config->get('sign_name');
             
         unset($data['sign_name']);
 
