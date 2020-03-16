af3421b93b74f9117c99a72d48e1f4886e2e8f7d
Alone88 <im@alone88.cn>
2020-03-16 23:35:04 +0800

    update styleci

--- tests/Gateways/UcloudGatewayTest.php
+++ tests/Gateways/UcloudGatewayTest.php
@@ -20,11 +20,12 @@ class UcloudGatewayTest extends TestCase
     public function testSend()
     {
         $config = [
-            'private_key' => '',//私钥
-            'public_key' => '',//公钥
-            'sig_content' => '',//签名
-            'project_id' => '',//默认不填，子账号才需要填
+            'private_key' => '', //私钥
+            'public_key' => '', //公钥
+            'sig_content' => '', //签名
+            'project_id' => '', //默认不填，子账号才需要填
         ];
+
         $gateway = \Mockery::mock(UcloudGateway::class . '[request]', [$config])->shouldAllowMockingProtectedMethods();
 
         $gateway->shouldReceive('request')->with(
@@ -46,8 +47,8 @@ class UcloudGatewayTest extends TestCase
         $message = new Message([
             'template' => '',
             'data' => [
-                'code' => '',// 如果是多个参数可以用数组
-                'mobiles' => '',//同时发送多个手机也可以用数组来,[1111111,11111]
+                'code' => '', // 如果是多个参数可以用数组
+                'mobiles' => '', //同时发送多个手机也可以用数组来,[1111111,11111]
             ]
         ]);
         $config = new Config($config);
