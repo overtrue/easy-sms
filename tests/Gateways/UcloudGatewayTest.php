c26a6f1650a58efba731bb3d620f802f4a50384d
Hyman <hyman.omg@gmail.com>
2023-04-13 20:39:37 +0800

    Ucloud网关支持模板指定签名

--- tests/Gateways/UcloudGatewayTest.php
+++ tests/Gateways/UcloudGatewayTest.php
@@ -69,4 +69,46 @@ class UcloudGatewayTest extends TestCase
 
         $gateway->send(new PhoneNumber(18888888888), $message, $config);
     }
+    
+    public function testSignContent()
+    {
+        $defaultSigContent = 'default_sig_content';
+        
+        $dataSigContent = 'data_sig_content';
+        
+        $config = [
+            'private_key' => '', //私钥
+            'public_key' => '', //公钥
+            'sig_content' => $defaultSigContent, //签名
+            'project_id' => '',
+        ];
+        $easySms = new EasySms($config);
+        
+        $phoneNumber = new PhoneNumber('18888888888');
+        $gateway = $easySms->gateway('ucloud');
+        
+        $reflectionMethod = new \ReflectionMethod(get_class($gateway), 'buildParams');
+        $reflectionMethod->setAccessible(true);
+        //验证指定签名
+        $message = new Message([
+            'template' => '',
+            'data' => [
+                'code' => '', // 如果是多个参数可以用数组
+                'mobiles' => '', //同时发送多个手机也可以用数组来,[1111111,11111]
+                'sig_content'=>$dataSigContent
+            ],
+        ]);
+        $result = $reflectionMethod->invokeArgs($gateway, [$phoneNumber, $message, $easySms->getConfig()]);
+        $this->assertSame($dataSigContent, $result['SigContent']);
+        //验证配置默认签名
+        $message = new Message([
+            'template' => '',
+            'data' => [
+                'code' => '', // 如果是多个参数可以用数组
+                'mobiles' => '', //同时发送多个手机也可以用数组来,[1111111,11111]
+            ],
+        ]);
+        $result = $reflectionMethod->invokeArgs($gateway, [$phoneNumber, $message, $easySms->getConfig()]);
+        $this->assertSame($defaultSigContent, $result['SigContent']);
+    }
 }
